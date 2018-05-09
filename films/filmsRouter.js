26c72f4c06bfab5e4b859390e453e7bf57f9c8b0
David Pok <david.pok@gmail.com>
2018-05-09 16:19:53 -0700

    attempted to start

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,10 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', function get(req, res) {
+    Film.find().then(films => {
+        res.status(200).json(films);
+    });
+});
 
 module.exports = router;
