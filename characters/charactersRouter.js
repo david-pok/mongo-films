f7f89dd32e2a5b85cd809775ce09e9bafc2fd974
David Pok <david.pok@gmail.com>
2018-05-10 16:29:23 -0700

    finished most of regular assignment

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,9 +1,50 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film');
+const Vehicle = require('../vehicles/Vehicle');
 
 const router = express.Router();
 
 // add endpoints here
+router.get('/', function(req, res) {
+    Character.find()
+    .then(chars => res.status(200).json(chars))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+router.get('/:id', function(req, res) {
+    Character.findById(req.params.id)
+    //^^^this is same as: Character.find({_id: req.params.id})
+    .populate('homeworld')
+    .then(char => {
+        Film.find({ characters: req.params.id })
+            .select('title')
+            .then(films => {
+                const character = { ...char._doc, movies: films };
+       
+        res.status(200).json(character)
+        });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
+
+router.get('/:id/vehicles', function(req, res) {
+    const { id } = req.params   
+       Vehicle.find({})
+       .populate('pilots')
+       .then(p => {
+        res.status(200).json(p)
+       })
+     
+   
+ 
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,9 +5,27 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
-router.get('/', function get(req, res) {
-    Film.find().then(films => {
-        res.status(200).json(films);
+
+router.get('/', function(req, res) {
+  let query = Film.find()
+    query.select('episode producer title director release_date')
+    .populate('characters', ' name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
+    .sort('episode')
+
+    const { producer, released } = req.query;
+//ONE WAY OF DOING REGEXP
+    if (producer) {
+      const filter = new RegExp(producer, 'i');
+      query.where({ producer: filter });
+    }
+  //ANOTHER WAY OF DOING REGEXP
+    if (released) {
+      query.where({ release_date: { $regex: released, $options: 'i' } });
+    }
+    query.then(films => res.status(200).json(films))
+    .catch(err => {
+        res.status(500).json(err);
     });
 });
 
